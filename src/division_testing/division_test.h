12ff473b286ec2f5f6e93abeba008cdffa438670
Weaveon <matteo.locatelli1993@gmail.com>
2016-06-11 10:50:19 +0200

    updated division testing

--- src/division_testing/division_test.h
+++ src/division_testing/division_test.h
@@ -9,10 +9,18 @@
 
 using namespace std;
 
-int number_of_needed_bits (int x, int divisor)
+int number_of_needed_bits(int h)
 {
-	int n = log2(x); //n is the precision of the input x, we assum q=n
+	if (h<=1){return 1;}
+	return floor( log2(h) + 1 );
+}
+
+
+int correct_rounding (int x, int divisor)
+{
+	int n = number_of_needed_bits(x); //n is the precision of the input x, we assum q=n
 	return n + 1 + ceil(log2(divisor)); 
 }
+
 
 #endif
--- src/division_testing/main.cpp
+++ src/division_testing/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include "division_test.h"
+#include <bitset>
+#include <cmath>
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	cout<<"If we want to make the operation 15/3"<<endl;
-	cout<<"We need "<<number_of_needed_bits(15,3)<<" bits for the result"<<endl;
+	int x = 15, b = 3;
+	int bits_needed = correct_rounding(x,b);
+	int first_shift, partial_result, final_result;
+	cout<<"If we want to make the operation "<<x<<"/"<<b<<endl;
+	cout<<"We need "<<bits_needed<<" bits for the result"<<endl;
+	//float result = computeMult(15,32,build(1,3));
+	first_shift = pow(2, bits_needed) * x;
+	cout<<"1) Shift to left "<<x<<" of "<<bits_needed<<" bits: "<<first_shift<<endl;
+	partial_result = first_shift / b;
+	cout<<"2) Divide the prevoius result for "<<b<<": "<<partial_result<<endl;
+	final_result = partial_result >> bits_needed;
+	cout<<"3) Shift to right the prevoius result of "<<bits_needed<<": "<<final_result<<endl;
 	return 0;
 }
