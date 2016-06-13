4d768318d168668595aaef4735e0e37010352652
Matteo Fusi <fusiled@gmail.com>
2016-06-13 15:01:15 +0200

    new branch?

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,48 @@
 
 #include "RationalConstants.h"
 #include "PeriodicRepresentation.h"
+#include "division_testing/division_test.h"
 #include "FixedPoint.h"
 #include <map>
 #include <iostream>
 #include <bitset>
 
 #define TEST_SET_SIZE 4
+#ifndef MAX_BITS 
+#define MAX_BITS 32
+#endif
 
 
 using namespace RationalConstants;
 using namespace std;
 
 
-int TestSet[][6]=
+long int TestSet[][7]=
 {
-	//a,   b,   e,   h,   p,   s
-	{ 5,   9,   0,   0,   35,  6},
-	{ 10,  30,  0,   0,   1,   2},
-	{ 13,  14, -1,   1,   6,   3},
-	{ 7,    3,  0,   2,   1,   2}
+	//a,   b,   e,   h,   p,   s  round
+	{ 15,  3,   0,   0,   35,  6,  8},
+	{ 30,  10,  0,   0,   1,   2,  7},
+	{ 14,  14, -1,   1,   6,   3,  4},
+	{ 5204485,    13,  0,   2,   1,   2,  6},
 };
 
 
 bool testBuild(int testElement[]);
 bool testDivision(int testElement[]);
+bool testShiftDivision(long int testElement[]);
 
 int main(int argc, char const *argv[])
 {
-	cout<<"Testing build function"<<endl;
 	for (int i=0; i<TEST_SET_SIZE; i++)
 	{
-		testBuild(TestSet[i]);
-	}
-
-	cout<<"Testing shift-and-add algorithm"<<endl;
-	for (int i=0; i<TEST_SET_SIZE; i++)
-	{
-		testDivision(TestSet[i]);
+		if (!testShiftDivision(TestSet[i]))
+		{
+			cout<<"test error! check "<<TestSet[0]<<"/"<<TestSet[1]<<endl;
+		}
+		else
+		{
+			cout<<"Test Successful"<<endl;
+		}
 	}
 
 
@@ -71,4 +76,28 @@ bool testDivision(int testElement[])
 		cout<<"computed result:"<<fp.toString()<<endl;
 		cout<<"raw result:   "<<fp.rawString()<<endl;
 	#endif
+}
+
+
+bool testShiftDivision(long int testElement[] )
+{
+	long int x = testElement[0];
+	long int b = testElement[1];
+	long int expectedResult=testElement[0]/testElement[1];
+	long int bits_needed = correct_rounding(x,b);
+	long int first_shift, partial_result, final_result;
+	first_shift = pow(2, bits_needed) * x;
+	partial_result = first_shift / b;
+	final_result = partial_result >> bits_needed;
+	#ifdef DEBUG
+		cout<<"We want to make the operation "<<x<<"/"<<b<<" = "<<expectedResult<<endl;
+		cout<<"bits_needed: "<<bits_needed<<endl;
+		cout<<"1) Shift to left "<<x<<" of "<<bits_needed<<" bits"<<endl;
+		cout<<"left_shift:     "<<bitset<MAX_BITS>(first_shift)<<endl;
+		cout<<"2) Divide the prevoius result for "<<b<<endl;
+		cout<<"partial_result: "<<bitset<MAX_BITS>(partial_result)<<endl;
+		cout<<"3) Shift to right the previous result of "<<bits_needed<<endl;
+		cout<<"final_result:   "<<bitset<MAX_BITS>(final_result)<<endl;
+	#endif
+	return expectedResult == final_result;
 }
